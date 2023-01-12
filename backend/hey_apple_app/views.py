7c3613a925c355780f5a832aac3921d1159c3c72
Sanmgmin <sangmins930@gmail.com>
2023-01-13 01:45:46 +0900

    feat: redis docker setting and edit API

--- backend/hey_apple_app/views.py
+++ backend/hey_apple_app/views.py
@@ -1,14 +1,14 @@
+import logging
 from rest_framework import viewsets
 from rest_framework.decorators import api_view  # @api_view 사용 가능
 from rest_framework.response import Response
 
 
 # from django.core.files.storage import FileSystemStorage #file(image) 관리
-from django.core.files.storage import default_storage #file 저장
-from django.http import JsonResponse # json형으로 반환
+from django.core.files.storage import default_storage  # file 저장
+from django.http import JsonResponse  # json형으로 반환
 
-
-
+from django.core.cache import cache
 
 from drf_yasg.utils import swagger_auto_schema  # swagger 적용
 
@@ -21,32 +21,45 @@ from .utils import *
 from .serializers import FruitSerializer
 
 
-
 # Create your views here.
 
 @api_view(['GET'])
 def get_fruit(request, id):
-    data = fruit.objects.get(id=id)
-    serializer = FruitSerializer(data)
-    return Response(serializer.data)
+    # data = cache.get(id)
+    # if data is None:
+    #     data = fruit.objects.get(id=id)
+    #     serializer = FruitSerializer(data)
+    #     cache.set(id, serializer.data)
+    #     print("get mysql")
+    #     return Response(serializer.data)
+    # print("get redis")
+    try:
+        data = cache.get_or_set(id, fruit.objects.get(id=id))  # timeout 설정 고민
+        serializer = FruitSerializer(data)
+        return Response(serializer.data)
+    except fruit.DoesNotExist as e:
+        logging.error(f"fruit_id: {id} does not exist")
+        return JsonResponse({"error": "error_page"})
+
 
 @api_view(['POST'])
 def get_order_bill(request):
-    uuidKey = str(uuid4()) #고유한 폴더명
-    imageName = str(uuid4()) # 입력받은 이미지만의 아이디 생성
+    uuidKey = str(uuid4())  # 고유한 폴더명
+    imageName = str(uuid4())  # 입력받은 이미지만의 아이디 생성
 
-    file = request.FILES['filename'] #입력받은 이미지
-    default_storage.save('ai_image/'+uuidKey+'/'+imageName+".jpg", file) #입력받은 이미지 저장 
+    file = request.FILES['filename']  # 입력받은 이미지
+    default_storage.save('ai_image/'+uuidKey+'/' +
+                         imageName+".jpg", file)  # 입력받은 이미지 저장
 
-    s3 = s3_connection() # s3 연결 확인
-    s3_upload = s3_put_object( # s3에 업로드 시도
-            s3, AWS_STORAGE_BUCKET_NAME,
-            '/backend/ai_image/' + uuidKey + '/' + imageName + '.jpg',
-            'image/' + imageName + '.jpg')
-    s3_url = s3_get_image_url(s3, 'image/' + str(imageName) + '.jpg') #업로드한 이미지 url 가져오기
+    s3 = s3_connection()  # s3 연결 확인
+    s3_upload = s3_put_object(  # s3에 업로드 시도
+        s3, AWS_STORAGE_BUCKET_NAME,
+        '/backend/ai_image/' + uuidKey + '/' + imageName + '.jpg',
+        'image/' + imageName + '.jpg')
+    s3_url = s3_get_image_url(
+        s3, 'image/' + str(imageName) + '.jpg')  # 업로드한 이미지 url 가져오기
 
     iImage = image()
     iImage.s3_image_url = s3_url
     iImage.s3_result_image_url = s3_url
     iImage.save()
-    
