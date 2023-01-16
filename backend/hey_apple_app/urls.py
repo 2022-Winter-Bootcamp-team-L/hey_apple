bbb4be2f5ec4bd92e2e83e94d666fbeda761e8c7
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-01-16 17:22:50 +0900

    셀러리 업데이트

--- backend/hey_apple_app/urls.py
+++ backend/hey_apple_app/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('fruits/<int:id>', views.get_fruit, name='fruits'),
 
     # image IO
-    path('orders', views.get_order_bill, name='orders'),
+    #path('orders', views.get_order_bill, name='orders'),
     path('orders/tasks', views.get_task_id),
 
-    path('orders', views.get_order_bill, name='orders'),
+    #path('orders', views.get_order_bill, name='orders'),
     path('bills', views.send_email_api),
 ]
