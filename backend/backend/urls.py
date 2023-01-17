391af34643db12278b6f108e293c73ab565ded64
SeongbinPark <binni0515@gmail.com>
2023-01-17 13:53:08 +0900

    feat(requirements.txt): add pymysql

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -41,7 +41,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # S
+    # Swagger
     path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
