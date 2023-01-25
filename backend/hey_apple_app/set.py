77f1d4e520875024e71c07d12e481e3af02690a0
sangminnim <sangmins930@gmail.com>
2023-01-25 21:07:46 +0900

    temporary elasticsearch

--- backend/hey_apple_app/set.py
+++ backend/hey_apple_app/set.py
@@ -1,42 +1,47 @@
+from backend.hey_apple_app.elastic import My_Elasticsearch
 from pymongo import MongoClient
-import os 
-from elasticsearch import Elasticsearch, helpers 
+import os
+from elasticsearch import Elasticsearch, helpers
 
-es = Elasticsearch(
-        hosts=['localhost'],
-        http_auth=('user','pass')
-    )
+es = My_Elasticsearch()
 
 
 def search_result():
     docs = es.search(
-            index = 'apple' , 
-            # body = {
-            #     "query" : {
-            #         "multi_match" : {
-            #             "query" : r_text,
-            #             "fields" : ["Category","Title", "Writer","Bookmade"]
-            #         }
-            #     }
-            # }
+        index='heyapple',
+        body={
+            "query": {
+                "multi_match": {
+                    "name": "사과"
+                }
+            }
+        }
+    )
+    result_dic = {}
+    e_result = []
+    for data in docs['hits']['hits']:
+        e_result.append(
+            {
+                "name": data["_source"]["name"],
+                "avg": data["_source"]["avg"],
+                "price1": data["_source"]["price1"],
+                "date1": data["_source"]["date1"],
+                "price2": data["_source"]["price2"],
+                "date2": data["_source"]["date2"],
+                "price3": data["_source"]["price3"],
+                "date3": data["_source"]["date3"],
+                "price4": data["_source"]["price4"],
+                "date4": data["_source"]["date4"],
+                "price5": data["_source"]["price5"],
+                "date5": data["_source"]["date5"],
+                "price6": data["_source"]["price6"],
+                "date6": data["_source"]["date6"],
+            }
         )
-    # result_dic = {}
-    # e_result = []
-    # for data in docs['hits']['hits']: 
-    #     e_result.append(
-    #         {
-    #             "Category" : data["_source"]["Category"],
-    #             "Title" : data["_source"]["Title"],
-    #             "Writer" : data["_source"]["Writer"],
-    #             "Bookmade" : data["_source"]["Book_made"],
-    #             "Sellprice" : data["_source"]["Sell_price"],
-    #             "ImageUri" : data["_source"]["Image_uri"]
-    #         }
-    #     )
-    # result = [e_result[0]]
+    result = [e_result[0]]
 
     result_dic = {
-        "result" : docs
+        "result": docs
     }
 
     return result_dic
