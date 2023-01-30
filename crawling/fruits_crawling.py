a289e05f346b078d3e9d132435fb1bee5b31f0f7
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-01-30 17:12:47 +0900

    scheduler 기능완료..

--- crawling/fruits_crawling.py
+++ crawling/fruits_crawling.py
@@ -12,128 +12,135 @@ from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium import webdriver
 from pyvirtualdisplay import Display
 
-chrome_ops = webdriver.ChromeOptions()
-# 원래는 --headless로 시뮬레이션 화면 설정을 꺼놓는게 기본, 하지만 끄게 되면 값이 다르게 크롤링 되는 현상 발생
-# chrome_ops.add_argument('--headless') 
-chrome_ops.add_argument('--no-sandbox')
-chrome_ops.add_argument('--disable-gpu')
-chrome_ops.add_argument('--disable-setuid-sandbox')
-chrome_ops.add_argument('--incognito')
-chrome_ops.add_argument('--disable-dev-shm-usage')
+def crawlingStart():
+    try: 
+        print("크롤링을 시작합니다...")
+        print("데이터 크롤링을 시작합니다....")
+        chrome_ops = webdriver.ChromeOptions()
+        # 원래는 --headless로 시뮬레이션 화면 설정을 꺼놓는게 기본, 하지만 끄게 되면 값이 다르게 크롤링 되는 현상 발생
+        # chrome_ops.add_argument('--headless') 
+        chrome_ops.add_argument('--no-sandbox')
+        chrome_ops.add_argument('--disable-gpu')
+        chrome_ops.add_argument('--disable-setuid-sandbox')
+        chrome_ops.add_argument('--incognito')
+        chrome_ops.add_argument('--disable-dev-shm-usage')
 
-display = Display(visible=0, size=(1420, 1080)) # 여기서 가상화면을 실행시키기 때문에 --headless 설정 안해도 됨
-display.start()
+        display = Display(visible=0, size=(1420, 1080)) # 여기서 가상화면을 실행시키기 때문에 --headless 설정 안해도 됨
+        display.start()
+        driver = webdriver.Chrome(options=chrome_ops)
+        driver.get('https://www.nongnet.or.kr/front/M000000048/content/view.do')
 
-driver = webdriver.Chrome(options=chrome_ops)
-driver.get('https://www.nongnet.or.kr/front/M000000048/content/view.do')
+        def get_info(num):
+            fruit_list = ['사과','배','포도','감귤','바나나','키위','파인애플','오렌지','레몬','망고','단감','아보카도']
+            eng_name_list = ['Apple','Pear','Grape','Mandarine','Banana','Kiwi','Pineapple','Orange',
+            'Lemon','Mango','Persimmon','Avocado']
+            weight_to_count = [1,1,2,10,5,4,0.3,4,3,1,2,2]
+
+            fruit = fruit_list[num]
+            eng_name = eng_name_list[num]
+
+            fruit_name = driver.find_element(By.XPATH, # 과일 이름 가져오기
+                r'//*[@id="gdid_selectPummokName"]').text
+            print(fruit_name)
+            if fruit == '망고' or fruit == '아보카도' or fruit == '배' : # 평균 가격 가져오기
+                price_avg = driver.find_element(By.XPATH, # 망고, 아보카도만 예외
+                    r'//*[@id="gcid_itemList"]/div/div/div/ul/li[1]/a/div/div/div/p/span/b').text
+            else :    
+                price_avg = driver.find_element(By.XPATH, # 평균 가격
+                    r'//*[@id="gcid_itemList"]/div/div/div[2]/ul/li[1]/a/div/div/div/p/span/b').text
+
+            price_trend = driver.find_elements(By.XPATH, # 6일 전까지 가격
+                r'//*[@id="selectDayTrendWithGubun"]/div/div/div[2]/ul')
+
+            print(price_avg.replace(',',''))
+            
+            price = int(price_avg.replace(',',''))
+            result_price = round(round(price / weight_to_count[num]),-2)
+
+            fruit_data = { 'name': eng_name, 'avg': result_price} # 크롤링 데이터 초기화
+            # fruit_data = {}
+            # fruit_data['name'] = fruit_name
+            # fruit_data['price'] = price_avg # 다른 방식 초기화
+
+            won = '' # N일 전 날짜 가격
+            i = 0 # 줄마다 해주어야 하는 동작이 다르기 때문에 줄 구분용 변수
+            id = 1
+            for info in price_trend: # li를 돌면서 
+                arr = info.text.split('\n')
+                for text in arr:
+                    if i%3 == 0:
+                        today = date.today() - timedelta(id-1)
+                        date_text = today.strftime('%m-%d')
+                        date_key = "date"+str(id)
+                        price_key = "price"+str(id)
+                        id = id+1
+                    elif i%3 == 1: # 가격만 남도록 파싱
+                        print(text)
+                        temp = text.split('톤')[1]
+                        won = int(temp.split('원')[0].replace(',',''))
+                    else :
+                        fruit_data[price_key] = round(round(won / weight_to_count[num]),-2)  # { 'price0' : '3400' }
+                        fruit_data[date_key] = date_text #{ 'date0' : '01.14(토 )'}
+                    i += 1
+            print(fruit_data)
+            frame = pd.DataFrame([fruit_data])
+            csv = 'DB_FRUITS.csv'
+            if not os.path.exists(csv): # 파일 생성 로직
+                frame.to_csv(csv, index=False, mode='w', encoding='utf-8-sig')
+            else:
+                frame.to_csv(csv, index=False, mode='a', encoding='utf-8-sig', header=False)
+            time.sleep(3)
+
+        def next_fruit(num):
+            fruit_list = ['사과','배','포도','감귤','바나나','키위','파인애플','오렌지','레몬','망고','단감','아보카도']
+            fruit = fruit_list[num]
+            main_search_ip = driver.find_element(By.XPATH, # 1. 메인 페이지의 검색창 클릭
+                r'//*[@id="content"]/div/div[2]/div[1]/div[2]/div[1]/div[1]/button')
+            main_search_ip.send_keys(Keys.ENTER)
+            time.sleep(0.5)
+            fruit_search_ip = driver.find_element(By.XPATH, # 2. 과일 종류 검색창 클릭
+                r'//*[@id="settingTab01"]/div[2]/input')
+            fruit_search_ip.send_keys(fruit)
+            fruit_search_ip.send_keys(Keys.ENTER)
+            time.sleep(0.5)
+
+            fruit_enter_bt = driver.find_element(By.XPATH, # 3. 과일 종류 검색 버튼 클릭
+                r'//*[@id="settingTab01"]/div[2]/button')
+            fruit_enter_bt.send_keys(Keys.ENTER)
+            time.sleep(0.5)
+
+            fruit_item_lb = driver.find_element(By.XPATH, # 4. 과일 종류 선택창에서 품목 라벨 클릭
+                r'//*[@id="mCSB_1_container"]/ul/li')
+            lb = fruit_item_lb.find_element(By.TAG_NAME,'label')
+            driver.execute_script('arguments[0].click();',lb) #얘는 왠지 .click 동작 안함, script로 강제 실행
+            time.sleep(0.5)    
+
+            fruit_save_bt = driver.find_element(By.XPATH, # 5. 저장 버튼 클릭
+                r'//*[@id="settingArea"]/ul/li[2]/button')
+            fruit_save_bt.send_keys(Keys.ENTER)
+            time.sleep(0.5)
+
+        def check_file():
+            file = './DB_FRUITS.csv'
+            print('check_file')
+            print(os.path.isfile(file))
+            if os.path.isfile(file):
+                os.remove(file)
+                print('이미 DB_FRUITS.csv파일이 있어 기존 파일을 삭제했습니다.')
 
 
-def get_info(num):
-    fruit_list = ['사과','배','포도','감귤','바나나','키위','파인애플','오렌지','레몬','망고','단감','아보카도']
-    eng_name_list = ['Apple','Pear','Grape','Mandarine','Banana','Kiwi','Pineapple','Orange',
-    'Lemon','Mango','Persimmon','Avocado']
-    weight_to_count = [1,1,2,10,5,4,0.3,4,3,1,2,2]
+        #craw
+        check_file()
 
-    fruit = fruit_list[num]
-    eng_name = eng_name_list[num]
-
-    fruit_name = driver.find_element(By.XPATH, # 과일 이름 가져오기
-        r'//*[@id="gdid_selectPummokName"]').text
-    print(fruit_name)
-    if fruit == '망고' or fruit == '아보카도' or fruit == '배' : # 평균 가격 가져오기
-        price_avg = driver.find_element(By.XPATH, # 망고, 아보카도만 예외
-            r'//*[@id="gcid_itemList"]/div/div/div/ul/li[1]/a/div/div/div/p/span/b').text
-    else :    
-        price_avg = driver.find_element(By.XPATH, # 평균 가격
-            r'//*[@id="gcid_itemList"]/div/div/div[2]/ul/li[1]/a/div/div/div/p/span/b').text
-
-    price_trend = driver.find_elements(By.XPATH, # 6일 전까지 가격
-        r'//*[@id="selectDayTrendWithGubun"]/div/div/div[2]/ul')
-
-    print(price_avg.replace(',',''))
+        for num in range(12):
+            next_fruit(num)
+            get_info(num)
+        print('크롤링 완료....')
+        driver.quit()
+        
+        return 1
     
-    price = int(price_avg.replace(',',''))
-    result_price = round(round(price / weight_to_count[num]),-2)
-
-    fruit_data = { 'name': eng_name, 'avg': result_price} # 크롤링 데이터 초기화
-    # fruit_data = {}
-    # fruit_data['name'] = fruit_name
-    # fruit_data['price'] = price_avg # 다른 방식 초기화
-
-    won = '' # N일 전 날짜 가격
-    i = 0 # 줄마다 해주어야 하는 동작이 다르기 때문에 줄 구분용 변수
-    id = 1
-    for info in price_trend: # li를 돌면서 
-        arr = info.text.split('\n')
-        for text in arr:
-            if i%3 == 0:
-                today = date.today() - timedelta(id-1)
-                date_text = today.strftime('%m-%d')
-                date_key = "date"+str(id)
-                price_key = "price"+str(id)
-                id = id+1
-            elif i%3 == 1: # 가격만 남도록 파싱
-                print(text)
-                temp = text.split('톤')[1]
-                won = int(temp.split('원')[0].replace(',',''))
-            else :
-                fruit_data[price_key] = round(round(won / weight_to_count[num]),-2)  # { 'price0' : '3400' }
-                fruit_data[date_key] = date_text #{ 'date0' : '01.14(토 )'}
-            i += 1
-    print(fruit_data)
-    frame = pd.DataFrame([fruit_data])
-    csv = 'DB_FRUITS.csv'
-    if not os.path.exists(csv): # 파일 생성 로직
-        frame.to_csv(csv, index=False, mode='w', encoding='utf-8-sig')
-    else:
-        frame.to_csv(csv, index=False, mode='a', encoding='utf-8-sig', header=False)
-    time.sleep(3)
-
-def next_fruit(num):
-    fruit_list = ['사과','배','포도','감귤','바나나','키위','파인애플','오렌지','레몬','망고','단감','아보카도']
-    fruit = fruit_list[num]
-    main_search_ip = driver.find_element(By.XPATH, # 1. 메인 페이지의 검색창 클릭
-        r'//*[@id="content"]/div/div[2]/div[1]/div[2]/div[1]/div[1]/button')
-    main_search_ip.send_keys(Keys.ENTER)
-    time.sleep(0.5)
-    fruit_search_ip = driver.find_element(By.XPATH, # 2. 과일 종류 검색창 클릭
-        r'//*[@id="settingTab01"]/div[2]/input')
-    fruit_search_ip.send_keys(fruit)
-    fruit_search_ip.send_keys(Keys.ENTER)
-    time.sleep(0.5)
-
-    fruit_enter_bt = driver.find_element(By.XPATH, # 3. 과일 종류 검색 버튼 클릭
-        r'//*[@id="settingTab01"]/div[2]/button')
-    fruit_enter_bt.send_keys(Keys.ENTER)
-    time.sleep(0.5)
-
-    fruit_item_lb = driver.find_element(By.XPATH, # 4. 과일 종류 선택창에서 품목 라벨 클릭
-        r'//*[@id="mCSB_1_container"]/ul/li')
-    lb = fruit_item_lb.find_element(By.TAG_NAME,'label')
-    driver.execute_script('arguments[0].click();',lb) #얘는 왠지 .click 동작 안함, script로 강제 실행
-    time.sleep(0.5)    
-
-    fruit_save_bt = driver.find_element(By.XPATH, # 5. 저장 버튼 클릭
-        r'//*[@id="settingArea"]/ul/li[2]/button')
-    fruit_save_bt.send_keys(Keys.ENTER)
-    time.sleep(0.5)
-
-def check_file():
-    file = './DB_FRUITS.csv'
-    print('check_file')
-    print(os.path.isfile(file))
-    if os.path.isfile(file):
-        os.remove(file)
-        print('이미 DB_FRUITS.csv파일이 있어 기존 파일을 삭제했습니다.')
-
-print("데이터 크롤링을 시작합니다....")
-
-check_file()
-
-for num in range(12):
-    next_fruit(num)
-    get_info(num)
-print('크롤링 완료....')
-driver.quit()
-
-
+    except:
+        print("fruit_crawling Error")
+        return 0
+    
---
+++ crawling/scheduler.py
@@ -0,0 +1,35 @@
+import schedule
+import time
+import datetime
+import fruits_crawling
+import pymongo_tut
+
+def crawlingFuc():
+    time_stamp()
+    print("Scheduler :: crawling Start")
+    crawling_result = 0
+    crawling_result =fruits_crawling.crawlingStart() # fruits_crawling start
+    print(crawling_result)
+    if crawling_result == 1 : #1 is sucess
+        pymongo_tut.mongoa() #start mongo_save
+        
+
+def time_stamp():
+    #now = time.localtime()
+    this_time = time.strftime('%Y.%m.%d - %H:%M:%S')
+    print("this_time : " , str(this_time))
+    
+def scheduler():
+    print("최초실행 시작 ....")
+    crawlingFuc()
+    
+    print ("스케줄러가 시작합니다")
+    schedule.every(5).seconds.do(time_stamp)
+    #schedule.every(300).seconds.do(crawlingFuc)
+    schedule.every().day.at("02:00").do(crawlingFuc)
+
+
+scheduler()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
